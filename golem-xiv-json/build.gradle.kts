a0dd7d2f6c0947e4effdf894bf8b270088cfe1e7
Kazik Pogoda <morisil@xemantic.com>
2025-05-31 19:28:18 +0200

    fix json module

--- golem-xiv-json/build.gradle.kts
+++ golem-xiv-json/build.gradle.kts
@@ -1,52 +1,12 @@
-@file:OptIn(ExperimentalKotlinGradlePluginApi::class)
-
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.plugin.power.assert)
-}
-
-//    jvm {
-//        // set up according to https://jakewharton.com/gradle-toolchains-are-rarely-a-good-idea/
-////        compilerOptions {
-////            apiVersion = kotlinTarget
-////            languageVersion = kotlinTarget
-////            jvmTarget = JvmTarget.fromTarget(javaTarget)
-////            freeCompilerArgs.add("-Xjdk-release=$javaTarget")
-////            progressiveMode = true
-////        }
-//    }
-
-kotlin {
-    compilerOptions {
-        //apiVersion = KotlinVersion.fromVersion(libs.versions.kotlinTarget.get())
-        //languageVersion = kotlinTarget
-        freeCompilerArgs.addAll(
-            "-Xmulti-dollar-interpolation",
-            "-opt-in=kotlin.uuid.ExperimentalUuidApi",
-            "-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
-            "-opt-in=kotlin.time.ExperimentalTime"
-        )
-        extraWarnings = true
-        progressiveMode = true
-    }
+    id("golem.convention")
 }
 
 dependencies {
     api(libs.kotlinx.coroutines.core)
 
     testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.xemantic.kotlin.test)
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-powerAssert {
-    functions = listOf(
-        "com.xemantic.kotlin.test.assert",
-        "com.xemantic.kotlin.test.have"
-    )
 }
