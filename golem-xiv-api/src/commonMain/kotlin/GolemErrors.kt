81d6f3bf6d22f38ed4305dd96cce4a8963a86ba6
Kazik Pogoda <morisil@xemantic.com>
2025-06-01 16:01:22 +0200

    first golem error added to api

---
+++ golem-xiv-api/src/commonMain/kotlin/GolemErrors.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2025. Kazimierz Pogoda / Xemantic. All rights reserved.
+ *
+ * This code is part of the "golem-xiv" project, a cognitive AI agent.
+ * Unauthorized reproduction or distribution is prohibited.
+ */
+
+package com.xemantic.ai.golem.api
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface GolemError {
+
+    @Serializable
+    @SerialName("noSuchCognitiveWorkspace")
+    data class NoSuchCognitiveWorkspace(
+        val workspaceId: Long
+    ) : GolemError
+
+}
