922dbf93bbc6881b6d246803b1ef15045fa99b2b
Kazik Pogoda <morisil@xemantic.com>
2025-12-30 18:08:04 +0100

    license update

--- golem-xiv-logging/src/main/kotlin/Logging.kt
+++ golem-xiv-logging/src/main/kotlin/Logging.kt
@@ -1,3 +1,21 @@
+/*
+ * Golem XIV - Autonomous metacognitive AI system with semantic memory and self-directed research
+ * Copyright (C) 2025  Kazimierz Pogoda / Xemantic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.xemantic.ai.golem.logging
 
 import org.slf4j.bridge.SLF4JBridgeHandler
--- golem-xiv-web/src/jsMain/kotlin/dev/DevMode.kt
+++ golem-xiv-web/src/jsMain/kotlin/dev/DevMode.kt
@@ -1,8 +1,19 @@
 /*
- * Copyright (c) 2025. Kazimierz Pogoda / Xemantic. All rights reserved.
+ * Golem XIV - Autonomous metacognitive AI system with semantic memory and self-directed research
+ * Copyright (C) 2025  Kazimierz Pogoda / Xemantic
  *
- * This code is part of the "golem-xiv" project, a cognitive AI agent.
- * Unauthorized reproduction or distribution is prohibited.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.xemantic.ai.golem.web.dev
